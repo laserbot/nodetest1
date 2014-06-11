5ef3c47ad20d65c0a84e3621e190ddc817440e93
Rocco <rockymoran@gmail.com>
2014-06-11 12:50:33 -0700

    Removing, starting over

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
-var server = app.listen(8080, function() {
-    console.log('Listening on port %d', server.address().port);
-});
